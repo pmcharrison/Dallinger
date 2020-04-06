9dc3c616e7f64ee7a42d4f3f1628ade7d0d931c7
Peter Harrison <pmc.harrison@gmail.com>
2020-04-06 12:07:19 +0100

    Fixed up non-adaptive demo

--- dallinger/deployment.py
+++ dallinger/deployment.py
@@ -282,7 +282,7 @@ def setup_experiment(log, debug=True, verbose=False, app=None, exp_config=None):
 
 INITIAL_DELAY = 5
 BACKOFF_FACTOR = 2
-MAX_ATTEMPTS = 4
+MAX_ATTEMPTS = 1
 
 
 def _handle_launch_data(url, error, delay=INITIAL_DELAY, attempts=MAX_ATTEMPTS):
